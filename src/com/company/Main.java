a58a62d39d6c056de904136543e2d7289314e87c
kwin555 <ktn555@att.net>
2018-09-16 20:19:54 -0500

    unique string checker

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Cac unique?: " + Main.isUniqueChars("Cac"));
+    }
+
+    static boolean isUniqueChars(String str) {
+        //assuming ASCII character set
+        //only 128 chars in ASCII set
+        if(str.length() > 128) return false;
+        boolean[] charSet = new boolean[128];
+        for(int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            if(charSet[val]) { //already found this char in string
+                return false;
+            }
+            charSet[val] = true;
+        }
+        return true;
+    }
+
+}
